da39a7e257230e22e2e59cad7ed20252718cf597
k-kiritaro <makotosiraisi3@gmail.com>
2021-11-30 00:02:47 +0900

    added breakout first

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
    return render_template('index.html') 
 
+@app.route("/breakout")
+def breakout():
+   return render_template('breakout.html') 
+
 if __name__ == "__main__":
     app.run()
